53fd0d01f17646ffcf103491dfe47f579ba8d0af
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-22 12:11:18 +0600

    Set up controls

--- src/PlaneCreator.ts
+++ src/PlaneCreator.ts
@@ -12,10 +12,13 @@ export class PlaneCreator {
     constructor(loader: TextureLoader, size: number, segments: number, positionX: number, positionY: number) {
         this.loader = loader;
 
-        const texture = this.loader.load("https://www.the3rdsequence.com/texturedb/download/26/texture/png/256/cracked+rock-256x256.png");
-        texture.repeat.set(size, size);
-        texture.wrapS = RepeatWrapping;
-        texture.wrapT = RepeatWrapping;
+        const texture = this.loader.load("https://www.the3rdsequence.com/texturedb/download/26/texture/png/256/cracked+rock-256x256.png",()=>{
+            texture.repeat.set(size, size);
+            texture.wrapS = RepeatWrapping;
+            texture.wrapT = RepeatWrapping;
+            texture.needsUpdate = true;
+        });
+
         this._plane = new Mesh(new PlaneGeometry(size, size, segments, segments), new MeshBasicMaterial({
 
             wireframe: false,
@@ -25,7 +28,7 @@ export class PlaneCreator {
         this._plane.position.set(positionX,positionY,0);
 
 
-        texture.needsUpdate = true;
+
 
     }
 
--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,51 @@ import {GUI} from "dat.gui";
 import {Line, Material, Mesh, MeshBasicMaterial, MeshPhongMaterial, PlaneGeometry} from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {LineGeometry} from "three/examples/jsm/lines/LineGeometry";
-import {noise} from "./libs/noise";
 import {Sky} from "three/examples/jsm/objects/Sky";
 import {PlaneCreator} from "./PlaneCreator";
-import {TrackballControls} from "three/examples/jsm/controls/TrackballControls";
+import {PointerLockControls} from "three/examples/jsm/controls/PointerLockControls";
 
 
 let canvas = document.querySelector("#c") as HTMLCanvasElement;
 const renderer = new THREE.WebGLRenderer({canvas});
 const gui = new GUI();
 const camera = new THREE.PerspectiveCamera( 40, 2, 0.1, 3000);
-const controls = new TrackballControls(camera,canvas);
-controls.target.set(0, 5, 0);
-controls.update();
+
+function getControls(camera:THREE.Camera, domElement: HTMLElement) {
+    const controls = new PointerLockControls(camera, domElement);
+    new OrbitControls(camera, domElement);
 
 
-camera.position.set(0,20,200);
-camera.lookAt(0,0,0)
+    const onKeyDown = function (event: KeyboardEvent) {
+        console.log(event.code);
+        switch (event.code) {
+
+            case 'KeyW':
+                controls.moveForward(.1)
+                break
+            case 'KeyA':
+                controls.moveRight(-.1)
+                break
+            case 'KeyS':
+                controls.moveForward(-.1)
+                break
+            case 'KeyD':
+                controls.moveRight(.1)
+                break
+        }
+    }
+    return onKeyDown;
+}
+
+const onKeyDown = getControls(camera,renderer.domElement);
+
+camera.position.set(0,5,0);
+camera.lookAt(1,1,1);
+camera.up.set(0,0,1);
+
+document.addEventListener("keydown", onKeyDown,false);
+
+
 
 const scene = new THREE.Scene();
 const loader = new THREE.TextureLoader();
