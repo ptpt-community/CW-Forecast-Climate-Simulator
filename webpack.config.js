c13e996623a2d3bf3746876f21f92272f5a5dba8
Labnan <khalidmasum@iut-dhaka.edu>
2021-07-26 23:21:40 +0600

    Add shading in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-module.exports ={
+module.exports = {
     mode: "development",
     entry: "./src/index.ts",
     devtool: "eval-source-map",
 
     module: {
-        rules:[
+        rules: [
             {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            include: [path.resolve(__dirname,'src')]
-         }
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                exclude: /node_modules/,
+                use: [
+                    'raw-loader'
+                ]
+            }
         ]
     },
 
@@ -20,14 +27,14 @@ module.exports ={
         extensions: ['.ts', '.js', '.json']
     },
 
-    output:{
-        filename:'bundle.js',
-        path:path.resolve(__dirname,'public')
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
     },
 
     devServer: {
         contentBase: path.join(__dirname, 'public'),
-      },
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './src/index.html',
