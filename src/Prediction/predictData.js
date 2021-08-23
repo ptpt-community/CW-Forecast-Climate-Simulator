0385b6dfba138b8d9e5b78e4c2834c5dfdb7d8d7
Sadman Saadat <sadmansaadat99@gmail.com>
2021-08-23 22:25:35 +0600

    simple prediction

---
+++ src/Prediction/predictData.js
@@ -0,0 +1,31 @@
+function linearRegression(y, x) {
+    var lr = {};
+    var n = y.length;
+    var sum_x = 0;
+    var sum_y = 0;
+    var sum_xy = 0;
+    var sum_xx = 0;
+    var sum_yy = 0;
+
+    for (var i = 0; i < y.length; i++) {
+
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += (x[i] * y[i]);
+        sum_xx += (x[i] * x[i]);
+        sum_yy += (y[i] * y[i]);
+    }
+
+    lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+    lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
+    lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
+
+    return lr;
+}
+
+var known_y = [1900, 2000, 2050, 2100];
+var known_x = [-1, 0, 2, 4];
+
+var output = linearRegression(known_y, known_x);
+
+console.log(output);
