87f164cd028c04a39463f0d86d18f88a263d85c0
Labnan <khalidmasum@iut-dhaka.edu>
2021-09-19 07:36:36 +0600

    Fix Quad Tree and Implement on Project

--- src/terrain_chunk/ChunkDirector/QuadTree.ts
+++ src/terrain_chunk/ChunkDirector/QuadTree.ts
@@ -3,7 +3,7 @@ import {Position} from "three/examples/jsm/utils/ShadowMapViewer";
 import {IChunkDirector} from "./IChunkDirector";
 import {ChunkRecord} from "../TerrainChunkManager";
 
-const _MIN_NODE_SIZE = 500;
+const _MIN_NODE_SIZE = 8;
 
 
 interface IQuadTree {
@@ -16,15 +16,15 @@ interface IQuadTree {
 
 export class QuadTree implements IChunkDirector{
 
-    private _root: IQuadTree;
+    private readonly _root: IQuadTree;
 
     constructor(
         params: {
-            min: Vector2,
-            max: Vector2
+            bottomLeft: Vector2,
+            topRight: Vector2
         }
     ) {
-        const box = new Box2(params.min, params.max);
+        const box = new Box2(params.bottomLeft, params.topRight);
         this._root = {
             bounds: box,
             children: [],
@@ -34,7 +34,8 @@ export class QuadTree implements IChunkDirector{
     }
 
 
-    getChildren() {
+
+    getNodes() {
         const children: IQuadTree[] = [];
         this._GetChildren(this._root, children);
         return children;
@@ -52,54 +53,53 @@ export class QuadTree implements IChunkDirector{
 
     }
 
-
-    getChunksFrom(position: Vector3):ChunkRecord[] {
+    getChunksFrom(position: Vector3) {
         this._Insert(this._root, new Vector2(position.x, position.z));
-        const children =  this.getChildren();
+
+        const nodes =  this.getNodes();
         const chunkRecords: ChunkRecord[] = []
-        children.map(child=>{
+        nodes.map(child=>{
             chunkRecords.push(new ChunkRecord(child.bounds));
         })
 
         return chunkRecords;
     }
 
+    private _Insert(node: IQuadTree, position: Vector2) {
+        const distanceToNode = this._DistanceToNode(node, position);
 
-    private _Insert(child: IQuadTree, position: Vector2) {
-        const distToChild = this._DistanceToChild(child, position);
+        if (distanceToNode < node.size.x && node.size.x > _MIN_NODE_SIZE) {
+            node.children = this._CreateChildren(node);
 
-        if (distToChild < child.size.x && child.size.x > _MIN_NODE_SIZE) {
-            child.children = this._CreateChildren(child);
-
-            for (let c of child.children) {
+            for (let c of node.children) {
                 this._Insert(c, position);
             }
         }
     }
 
-    private  _DistanceToChild(child: IQuadTree, position: Vector2) {
-        return child.center.distanceTo(position);
+    private _DistanceToNode(node: IQuadTree, position: Vector2) {
+        return node.center.distanceTo(position);
     }
 
 
-    private _CreateChildren(child: IQuadTree) {
-        const midpoint = child.bounds.getCenter(new Vector2());
+    private _CreateChildren(node: IQuadTree) {
+        const midpoint = node.bounds.getCenter(new Vector2());
 
         // Bottom left
-        const bottomLeft = new Box2(child.bounds.min, midpoint);
+        const bottomLeft = new Box2(node.bounds.min, midpoint);
 
         // Bottom right
         const bottomRight = new Box2(
-            new Vector2(midpoint.x, child.bounds.min.y),
-            new Vector2(child.bounds.max.x, midpoint.y));
+            new Vector2(midpoint.x, node.bounds.min.y),
+            new Vector2(node.bounds.max.x, midpoint.y));
 
         // Top left
         const topLeft = new Box2(
-            new Vector2(child.bounds.min.x, midpoint.y),
-            new Vector2(midpoint.x, child.bounds.max.y));
+            new Vector2(node.bounds.min.x, midpoint.y),
+            new Vector2(midpoint.x, node.bounds.max.y));
 
         // Top right
-        const topRight = new Box2(midpoint, child.bounds.max);
+        const topRight = new Box2(midpoint, node.bounds.max);
 
         const children = [bottomLeft, bottomRight, topLeft, topRight].map(
             box => {
