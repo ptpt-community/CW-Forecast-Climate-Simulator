caeefc4d8f213a280f6c67f3186c0c7c315e18c8
Jisan <jishananam@iut-dhaka.edu>
2021-10-01 16:49:06 +0600

    Shameless color green :3  #10 #19

--- src/shaders/ground/fragment.glsl
+++ src/shaders/ground/fragment.glsl
@@ -1,7 +1,6 @@
-
 varying vec4 vPosition;
 varying float vSnoise;
-
+vec3 biomeColor[10];
 float cold_desert_slope = 1.3636;
 float woodland_slope = 5.4545;
 float seasonal_forest = 10.00;
@@ -11,27 +10,27 @@ float tropical_seasonal_forest = 5.00;
 float tropical_rain_forest = 7.69;
 
 
-float check_secondPhase(float temperature, float precipitation){
+int check_secondPhase(float temperature, float precipitation){
     float a=precipitation/22.00;
-    if (a<=cold_desert_slope)return 3.00;
-    else if (a>cold_desert_slope&&a<=woodland_slope)return 4.00;
-    else if (a>woodland_slope&&a<=seasonal_forest)return 5.00;
-    else if (a>seasonal_forest&&a<=temparature_rain_forest)return 6.00;
-    return 1.00;
+    if (a<=cold_desert_slope)return 3;
+    else if (a>cold_desert_slope&&a<=woodland_slope)return 4;
+    else if (a>woodland_slope&&a<=seasonal_forest)return 5;
+    else if (a>seasonal_forest&&a<=temparature_rain_forest)return 6;
+    return 1;
 }
-float check_thirdPhase(float temperature, float precipitation){
+int check_thirdPhase(float temperature, float precipitation){
     float a=precipitation-temperature*(60.00/13.00);
-    if (a<61.538)return 7.00;
-    else if (a>=61.538&&a<118.461)return 8.00;
-    return 9.00;
+    if (a<61.538)return 7;
+    else if (a>=61.538&&a<118.461)return 8;
+    return 9;
 }
-float getBiome(float temperature, float precipitation){
+int getBiome(float temperature, float precipitation){
 
-    if (temperature< -2.00)return 1.00;
-    else if (temperature> -2.00&&temperature<=7.00)return 2.00;
+    if (temperature< -2.00)return 1;
+    else if (temperature> -2.00&&temperature<=7.00)return 2;
     else if (temperature>7.00&&temperature<18.00)return check_secondPhase(temperature, precipitation);
     else if (temperature>=18.00&&temperature<33.00) return check_thirdPhase(temperature, precipitation);
-    return 8.00;
+    return 8;
 
 }
 vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
@@ -67,14 +66,14 @@ float simplex(vec2 v){
 
 float getTemperature(float offset){
     //Temperature = sin(x) ranging from -10 to + 35 which is  -22.5 to + 22.5  and offsetted +12.5
-    float temperature =  sin(distance(vec2(0,0),vPosition.xz/20.0));
+    float temperature =  sin(distance(vec2(0, 0), vPosition.xz/50.0));
     temperature -= vPosition.y*.1; /**IMPORTANT!!Need Research*/
     return temperature*22.5 + 12.5 + offset;
 }
 
 float getPreceipitationMath(float temp){
     float f = 1.25*temp - 30.0;
-    float precipitation = 10.0*f -pow(10.0,0.2*f) + 400.0;
+    float precipitation = 10.0*f -pow(10.0, 0.2*f) + 400.0;
     return precipitation<0.0? 0.0: precipitation;
 }
 float getPrecipitation(float temperature){
@@ -87,7 +86,15 @@ float getPrecipitation(float temperature){
 
 void main(){
 
-    vec3 highColor = vec3(1.0,0.5,0.3);
+    biomeColor[1]=vec3(.5, .2, .7);
+    biomeColor[2]=vec3(.2, .8, .2);
+    biomeColor[3]=vec3(.3, .9, .3);
+    biomeColor[4]=vec3(.6, .4, .9);
+    biomeColor[5]=vec3(.6, .5, .1);
+    biomeColor[6]=vec3(.1, .2, .8);
+    biomeColor[7]=vec3(.7, .8, .9);
+    biomeColor[8]=vec3(.7, .8, .5);
+    vec3 highColor = vec3(1.0, 0.5, 0.3);
     vec3 lowColor = vec3(0.3, 0.3, .5);
 
     float temperature = getTemperature(0.0);
@@ -98,12 +105,11 @@ void main(){
     float precipitationStrength = precipitation*.0025;
 
     float mixStrength =precipitationStrength;
-    float biomeCofficient = getBiome(temperature,precipitation);
+
+    int index=getBiome(temperature,precipitation);
+
+    vec3 color =biomeColor[index];
 
 
-
-    vec3 color = mix(lowColor*biomeCofficient, highColor*biomeCofficient, mixStrength);
-
-
-    gl_FragColor = vec4(color,.8);
+    gl_FragColor = vec4(color, .8);
 }
